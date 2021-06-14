4d6b8ccc03faff0bd4dee64d19c05aa3b0ea619a
Andree Jimenez <andree_jimenez@outlook.com>
2021-06-14 00:47:51 -0500

    Fix map init

--- scripts/index.js
+++ scripts/index.js
@@ -29,100 +29,97 @@ const configuraMenu = (user) => {
 const propertiesList = document.getElementById("propertiesList");
 const mapControls = document.querySelector(".mapControls");
 
+function iniciaMapa() {
+  var btnroadmap = document.getElementById("btnroadmap");
+  var btnsatelite = document.getElementById("btnsatellite");
+  var btnhybrid = document.getElementById("btnhybrid");
+  var btnterrain = document.getElementById("btnterrain");
+
+  var coordenadas = {
+    lat: 23.2401797,
+    lng: -106.4331511,
+  };
+
+  var localidades = [];
+
+  fetch("properties.json").then(function (response) {
+    response.json().then(function (data) {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordenadas,
+        zoom: 3,
+      });
+
+      data.forEach((markerFetch) => {
+        var information =
+          "<strong>Name: </strong>" +
+          markerFetch.name +
+          "| <strong>casos: </strong> " +
+          markerFetch.category;
+
+        var infoWindow = new google.maps.InfoWindow({
+          content: information,
+        });
+
+        let marker = new google.maps.Marker({
+          map: map,
+          position: new google.maps.LatLng(
+            markerFetch.location.latitude,
+            markerFetch.location.longitude
+          ),
+          title: markerFetch.name + registro.category,
+        });
+
+        marker.addListener("click", function () {
+          infoWindow.open(map, marker);
+        });
+      });
+    });
+  });
+
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var marcadores = [];
+  var cuenta = 1;
+
+  localidades.forEach((localidad) => {
+    console.log(localidad);
+
+    let marcador = new google.maps.Marker({
+      map: map,
+      position: localidad,
+      label: labels[cuenta % labels.length],
+    });
+
+    marcadores.push(marcador);
+    cuenta++;
+  });
+
+  var markerCluster = new MarkerClusterer(map, marcadores, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    gridSize: 60,
+    maxZoom: 10,
+  });
+
+  btnroadmap.addEventListener("click", function () {
+    map.setMapTypeId("roadmap");
+  });
+
+  btnsatelite.addEventListener("click", function () {
+    map.setMapTypeId("satellite");
+  });
+
+  btnhybrid.addEventListener("click", function () {
+    map.setMapTypeId("hybrid");
+  });
+
+  btnterrain.addEventListener("click", function () {
+    map.setMapTypeId("terrain");
+  });
+}
+
 const getProperties = (data) => {
   if (data.length) {
     let html = "";
-
-    var btnroadmap = document.getElementById("btnroadmap");
-    var btnsatelite = document.getElementById("btnsatellite");
-    var btnhybrid = document.getElementById("btnhybrid");
-    var btnterrain = document.getElementById("btnterrain");
-
-    var coordenadas = {
-      lat: 23.2401797,
-      lng: -106.4331511,
-    };
-
-    var localidades = [];
-    // data.forEach((doc) => {
-    //   localidades.push({lat: doc.data().Location.Latitud, lng: doc.data().Location.Longitud})
-    // });
-
-    function iniciaMapa() {
-      fetch("properties.json").then(function (response) {
-        response.json().then(function (data) {
-          const map = new google.maps.Map(document.getElementById("map"), {
-            center: coordenadas,
-            zoom: 3,
-          });
-
-          data.forEach((markerFetch) => {
-            var information =
-              "<strong>Name: </strong>" +
-              markerFetch.name +
-              "| <strong>casos: </strong> " +
-              markerFetch.category;
-
-            var infoWindow = new google.maps.InfoWindow({
-              content: information,
-            });
-
-            let marker = new google.maps.Marker({
-              map: map,
-              position: new google.maps.LatLng(
-                markerFetch.location.latitude,
-                markerFetch.location.longitude
-              ),
-              title: markerFetch.name + registro.category,
-            });
-
-            marker.addListener("click", function () {
-              infoWindow.open(map, marker);
-            });
-          });
-        });
-      });
-
-      var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      var marcadores = [];
-      var cuenta = 1;
-
-      localidades.forEach((localidad) => {
-        console.log(localidad);
-
-        let marcador = new google.maps.Marker({
-          map: map,
-          position: localidad,
-          label: labels[cuenta % labels.length],
-        });
-
-        marcadores.push(marcador);
-        cuenta++;
-      });
-
-      var markerCluster = new MarkerClusterer(map, marcadores, {
-        imagePath:
-          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-        gridSize: 60,
-        maxZoom: 10,
-      });
-
-      btnroadmap.addEventListener("click", function () {
-        map.setMapTypeId("roadmap");
-      });
-
-      btnsatelite.addEventListener("click", function () {
-        map.setMapTypeId("satellite");
-      });
-
-      btnhybrid.addEventListener("click", function () {
-        map.setMapTypeId("hybrid");
-      });
-
-      btnterrain.addEventListener("click", function () {
-        map.setMapTypeId("terrain");
-      });
-    }
 
     data.forEach((doc) => {
       const properties = doc.data();
@@ -137,17 +134,17 @@ const getProperties = (data) => {
 
       html += columna;
     });
-    
-    body.classList.remove('overflowHidden');
-    mapControls.classList.remove('displayNone');
+
+    body.classList.remove("overflowHidden");
+    mapControls.classList.remove("displayNone");
 
     propertiesList.innerHTML = html;
   } else {
-    body.classList.add('overflowHidden');
-    mapControls.classList.add('displayNone');
+    body.classList.add("overflowHidden");
+    mapControls.classList.add("displayNone");
 
-    propertiesList.innerHTML =`
+    propertiesList.innerHTML = `
       <p class="alternativeText">Please Log in to see the properties.</p>
-      <img class="cityscape" src="./images/cityLandscape.svg"/>`
+      <img class="cityscape" src="./images/cityLandscape.svg"/>`;
   }
 };
